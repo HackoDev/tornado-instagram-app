134da666b8cad9450ba16dc6acadcfc30f2bc267
Evgeniy Hacko <79889501003@yandex.ru>
2017-10-31 13:18:59 +0300

    refactor project structure

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# tornado-instagram-app
+## Test project with Tornado / MongoDB / Instagram API
+
+### Functionality
+
+- token authentication
+- registration new user by instagram
+- registration new user by username/password pairs
+- attach your instagram account
+- list and select instagram account for user current session
+- switching between existing accounts
+- searching public profiles by instagram api
+- searching username in the system
+- chat with users
+- searching media contents by text / tags
+
+
+### First steps:
+
+- create virtualenv for `python 3.5`
+- install and start MongoDB service
+- run tornado app on 8888 port: ``` python instagram-app/app.py ```
---
+++ instagram-app/tests/test_handlers.py
@@ -0,0 +1,45 @@
+import json
+from tornado.testing import AsyncHTTPTestCase
+
+from app import make_app
+
+
+app = make_app()
+
+
+def clear_db(app=None):
+    app.settings['db'].users.remove()
+    app.settings['db'].tokens.remove()
+
+
+class TestHandlerBase(AsyncHTTPTestCase):
+    def setUp(self):
+        clear_db(self.get_app())
+        super(TestHandlerBase, self).setUp()
+
+    def get_app(self):
+        return app
+
+    def get_http_port(self):
+        return 8888
+
+
+class TestBucketHandler(TestHandlerBase):
+
+    def test_create_something_test(self):
+
+        data = {
+            'client_info': 'curl agent',
+            'platform': 'web'
+        }
+        response = self.fetch(
+            '/api/handshake',
+            headers={'Content-Type': 'application/json'},
+            method='POST',
+            body=json.dumps(data),
+            follow_redirects=False
+        )
+
+        self.assertEqual(response.code, 200)
+        response_json = json.loads(response.body.decode())
+        self.assertIn('token', response_json)
